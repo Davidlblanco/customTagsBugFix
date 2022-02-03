94bbce5e52339058fcf57c2b6b90b097d79025c0
SavioChermont <saviochwt@gmail.com>
2022-02-03 17:49:53 -0300

    feat: add validation for promotion date and conditions

--- react/components/FreightBar/api/getPromotionData.ts
+++ react/components/FreightBar/api/getPromotionData.ts
@@ -1,25 +1,25 @@
 const getPromotionData = async (PROMOTION_ID: String) => {
-    let promotionData = {};
-    const url = `/api/rnb/pvt/calculatorconfiguration/${PROMOTION_ID}`;
+  let promotionData = {};
+  const url = `/api/rnb/pvt/calculatorconfiguration/${PROMOTION_ID}`;
 
-    const header = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
-        'x-vtex-api-appKey': 'vtexappkey-simanqa-GPTOSK',
-        'x-vtex-api-appToken': 'UZYMGCVGGKUUCVYKCZTZQYLLJTCOXIADEFYBBXQJIZFMUBINSWAWUFIVOMDNZHJXKRQXXXJEJJRRTYQHKPWBIEIYWCNOUDKBPHBDKLQVMWNKORCPWVCKKIPFIRAVJVKG'
-    };
+  const header = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json; charset=utf-8',
+    'x-vtex-api-appKey': 'vtexappkey-simanqa-GPTOSK',
+    'x-vtex-api-appToken': 'UZYMGCVGGKUUCVYKCZTZQYLLJTCOXIADEFYBBXQJIZFMUBINSWAWUFIVOMDNZHJXKRQXXXJEJJRRTYQHKPWBIEIYWCNOUDKBPHBDKLQVMWNKORCPWVCKKIPFIRAVJVKG'
+  };
 
-    const options = {
-        method: 'GET',
-        headers: header
-    };
+  const options = {
+    method: 'GET',
+    headers: header
+  };
 
-    await fetch(url, options)
-        .then(res => res.json())
-        .then(json => promotionData = json)
-        .catch(err => console.error('error:' + err));
+  await fetch(url, options)
+    .then(res => res.json())
+    .then(json => promotionData = json)
+    .catch(err => console.error('error:' + err));
 
-    return promotionData;
+  return promotionData;
 }
 
 export default getPromotionData;
