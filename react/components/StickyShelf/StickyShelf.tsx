596c70d47e1061dc31fd70518ef3b01382a79d2f
Daniel Carvalho <daniel.carvalho.ti@outlook.com>
2022-03-08 16:22:05 -0300

    fix: review component stickyshelf

--- react/components/StickyShelf/StickyShelf.tsx
+++ react/components/StickyShelf/StickyShelf.tsx
@@ -6,64 +6,64 @@ import { canUseDOM } from "vtex.render-runtime";
 import styles from "./StickyShelf.css";
 
 const StickyShelf: StorefrontFunctionComponent = ({ children }: any) => {
-    if (!canUseDOM) return <></>;
+  if (!canUseDOM) return <></>;
 
-    const pathName = window.location.pathname;
-    const [open, setOpen] = useState(true);
-    const [show, setShow] = useState(true);
+  const pathName = window.location.pathname;
+  const [open, setOpen] = useState(true);
+  const [show, setShow] = useState(false);
 
-    useEffect(() => {
-        setOpen(pathName === "/" ? true : false);
-        setShow(pathName.includes("orderPlaced") ? false : true);
-    }, [pathName]);
+  useEffect(() => {
+    setOpen(pathName === "/" ? true : false);
+    setShow(pathName.includes("orderPlaced") ? false : true);
+  }, [pathName]);
 
-    return show ? (
-        <div className={styles.containerStickyShelf}>
-            <div
-                className={styles.containerStickyShelfClose}
-                onClick={() => setOpen(!open)}
-                style={
-                    open
-                        ? { opacity: 0, visibility: "hidden" }
-                        : {
-                              opacity: 1,
-                              visibility: "visible"
-                          }
-                }
-            >
-                <div className={styles.containerLogo}>
-                    <ThunderBlack />
-                    <h3 className={styles.closeText}>ON SALE</h3>
-                </div>
-            </div>
-
-            <div
-                className={styles.containerStickyShelfOpen}
-                style={
-                    open
-                        ? {
-                              opacity: 1,
-                              visibility: "visible"
-                          }
-                        : { opacity: 0, visibility: "hidden" }
-                }
-            >
-                <div className={styles.containerHeader}>
-                    <div className={styles.containerHeaderLogo}>
-                        <ThunderWhite />
-                        <h3 className={styles.openText}>ON SALE</h3>
-                    </div>
-                    <CloseIcon
-                        onClick={() => setOpen(!open)}
-                        style={{ cursor: "pointer" }}
-                    />
-                </div>
-                {children}
-            </div>
+  return show ? (
+    <div className={styles.containerStickyShelf}>
+      <div
+        className={styles.containerStickyShelfClose}
+        onClick={() => setOpen(!open)}
+        style={
+          open
+            ? { opacity: 0, visibility: "hidden" }
+            : {
+              opacity: 1,
+              visibility: "visible"
+            }
+        }
+      >
+        <div className={styles.containerLogo}>
+          <ThunderBlack />
+          <h3 className={styles.closeText}>ON SALE</h3>
         </div>
-    ) : (
-        <></>
-    );
+      </div>
+
+      <div
+        className={styles.containerStickyShelfOpen}
+        style={
+          open
+            ? {
+              opacity: 1,
+              visibility: "visible"
+            }
+            : { opacity: 0, visibility: "hidden" }
+        }
+      >
+        <div className={styles.containerHeader}>
+          <div className={styles.containerHeaderLogo}>
+            <ThunderWhite />
+            <h3 className={styles.openText}>ON SALE</h3>
+          </div>
+          <CloseIcon
+            onClick={() => setOpen(!open)}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        {children}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export { StickyShelf };
