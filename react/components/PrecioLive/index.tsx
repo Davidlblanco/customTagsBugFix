cc3013a501beee2706aab45302c5055a75c5f0f5
SavioChermont <saviochwt@gmail.com>
2022-05-31 14:30:14 -0300

    fix: refactor Precio Live component

--- react/components/PrecioLive/index.tsx
+++ react/components/PrecioLive/index.tsx
@@ -1,213 +1,41 @@
 import React, { useEffect, useState } from "react";
 import waitForEl from "../../utils/waitForEl";
 import styles from "./index.css";
-import { useOrderForm } from "vtex.order-manager/OrderForm";
-
-/**
- * @example
- * arraysEqual([{productId: '10'}], [{productId: '10'}]); // Return true;
- *
- * @param array1  {any[]} required - Array 1, a ordem dos parametos independe
- * @param  array2 {any[]} required - Array 2, a ordem dos parametos independe
- * @returns arrayIsEqual {boolean}
- */
-function arraysEqual(array1: any[], array2: any[]): boolean {
-    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
-    return JSON.stringify(array1) == JSON.stringify(array2);
-}
-
-/**
- * @example
- * whoItemIsDifferent([{productId: '10'}, {productId: '20'}], [{productId: '10'}]); // Return '20';
- *
- * @param items_main  {any[]} required - Array de items que vai ser percorrido (deve ser o de maior tamanho)
- * @param  items_secondary {any[]} required - Array de items que vai ser comparado (deve ser o de menor tamanho)
- * @returns item_id {string}
- */
-function whoItemIsDifferent(items_main: any[], items_secondary: any[]): string {
-    let itemDifferent = "";
-
-    items_main.forEach(item_main => {
-        let isDiff = true;
-
-        items_secondary.forEach(item_secondary => {
-            if (item_main.id == item_secondary.id) {
-                isDiff = false;
-                return;
-            }
-        });
-        if (isDiff == true) {
-            itemDifferent = item_main.id;
-            return;
-        }
-    });
-
-    return itemDifferent;
-}
-
-/**
- * @example
- * whoItemQuantityChanged([{productId: '10', quantity: 2}, {productId: '20', quantity: 1}], [{productId: '10', quantity: 1}, {productId: '20', quantity: 1}]); // Return 0;
- *
- * @param items  {any[]} required - Array de items após a mudança de quantidade
- * @param  previous_items {any[]} required - Array de items antes da mudança de quantidade
- * @returns index on array of item that changed quantity {Number}
- */
-function indexOfItemQuantityChanged(
-    items: any[],
-    previous_items: any[]
-): number {
-    let indexOfItem = -1;
-
-    items.forEach((item_main, index) => {
-        if (item_main.quantity != previous_items[index].quantity) {
-            indexOfItem = index;
-            return;
-        }
-    });
-
-    return indexOfItem;
-}
+import { useDevice } from "vtex.device-detector";
 
 const PrecioLive = () => {
-    const {
-      orderForm: { id: orderformId, items, customData: defaultCustomData }
-  } = useOrderForm();
-
     const [inserted, setInserted] = useState(false);
-    const [previousItems, setPreviousItems] = useState<null | any[]>(null);
-    const [customData, setCustomData] = useState<any>(defaultCustomData);
-
-    useEffect(() => {
-        if (previousItems == null) setPreviousItems(items);
-        else {
-            if (arraysEqual(previousItems, items)) return;
-
-            if (previousItems.length > items.length) {
-                //First case: item removed to order
-                const itemIdRemoved = whoItemIsDifferent(previousItems, items);
-                removeItemOnCustomData(itemIdRemoved);
-            } else if (previousItems.length < items.length) {
-                //Second case: item added to order
-                const itemIdAdded = whoItemIsDifferent(items, previousItems);
-                addItemOnCustomData(itemIdAdded);
-            } else {
-                //Third case: Some item change the quantity
-                const indexOfItemChanged = indexOfItemQuantityChanged(
-                    items,
-                    previousItems
-                );
-
-                if (indexOfItemChanged < 0) return;
-
-                const item = items[indexOfItemChanged],
-                    previous_item = previousItems[indexOfItemChanged];
-
-                if (item.quantity > previous_item.quantity) {
-                    const productsNameElements = document.querySelectorAll(
-                        ".vtex-product-summary-2-x-productBrand"
-                    );
-                    if (productsNameElements) {
-                        const productsName = Array.from(
-                            productsNameElements
-                        ).map(el => el.textContent);
-
-                        const [productname] = productsName.filter(name => name?.includes(item.skuName));
-
-                        if (productname) addItemOnCustomData(item.id);
-                            
-                    }
-                }
-            }
-
-            setPreviousItems(items);
-        }
-    }, [items]);
+    const { isMobile } = useDevice();
 
     useEffect(() => {
         if (!inserted) {
             setInserted(true);
             waitForEl(".vtex-product-price-1-x-sellingPrice", insertPrecioLive);
         }
-    }, []);
+    }, [inserted]);
 
-    function addItemOnCustomData(productId: string) {
-        let newCustomDataItems: string[] = []; 
-        let alreadyContainItem = false;
-
-        if (customData) {
-            const [precioLiveApp] = customData.customApps.filter(
-                (app: any) => app.id == "preciolive"
+    useEffect(() => {
+        if (isMobile)
+            waitForEl(
+                ".vtexventures-livestreaming-free-0-x-player-button",
+                resetPrecioLive
             );
+    }, [isMobile]);
 
-            if (precioLiveApp) {
-              console.log("precioLiveApp: ", precioLiveApp, customData);
-              const customDataItems = JSON.parse(precioLiveApp.fields.items);
-                alreadyContainItem = customDataItems.includes(
-                    productId
-                );
-
-                if (alreadyContainItem) return;
-                newCustomDataItems = customDataItems;
-              }
-        }
-        newCustomDataItems.push(productId);
-
-        setPrecioLiveCustomData(newCustomDataItems);
-    }
-
-    function removeItemOnCustomData(productId: string) {
-        let newCustomDataItems: any[] = [],
-            alreadyContainItem = false;
-
-        if (customData) {
-            const [precioLiveApp] = customData.customApps.filter(
-                (app: any) => app.id == "preciolive"
-            );
-
-            if (precioLiveApp) {
-              const customDataItems = JSON.parse(precioLiveApp.fields.items);
-                alreadyContainItem = customDataItems.includes(
-                    productId
-                );
-
-                if (alreadyContainItem) {
-                    for(let i = 0; i < customDataItems.length; i++) {
-                      if (customDataItems[i] != productId) newCustomDataItems.push(customDataItems[i]);
-                    }
-                }
-            }
-            setPrecioLiveCustomData(newCustomDataItems);
-        }
-    }
-
-  function setPrecioLiveCustomData(items: any[]) {
-      const options = {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json"
-          },
-          body: JSON.stringify({ items: JSON.stringify(items) })
-      };
-
-      fetch(
-          `/api/checkout/pub/orderForm/${orderformId}/customData/preciolive`,
-          options
-      )
-          .then(response => response.json())
-          .then(response => {setCustomData(response.customData ? response.customData : {})})
-          .catch(err => console.error("Precio Live Custom Data error: ", err));
-  }
-
-    const insertPrecioLive = (elements: any) => {
+    function insertPrecioLive(elements: any) {
+        console.log("Inserindo tag!");
+        if (document.querySelector(".siman-m3-custom-0-x-precio-live")) return;
         Array.from(elements).map((el: any) => {
             const span = document.createElement("span");
             span.innerText = "Precio Live";
             span.classList.add(styles["precio-live"]);
             el.append(span);
         });
-    };
+    }
+
+    function resetPrecioLive(elements: any) {
+        elements[0].addEventListener("click", () => setInserted(false));
+    }
 
     return <></>;
 };
