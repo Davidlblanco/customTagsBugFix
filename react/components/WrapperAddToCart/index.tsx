a292690c1d0ef4f80217e83e4a91e88e07e3d4c0
SavioChermont <saviochwt@gmail.com>
2022-07-27 13:29:48 -0300

    feat: add wrapper add to cart component

---
+++ react/components/WrapperAddToCart/index.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react'
+import { useProduct } from 'vtex.product-context'
+
+interface WrapperAddToCartProps {
+  ProductQuantity: React.ComponentType 
+  AddToCartWithoutSimanPro: React.ComponentType
+  AddToCartWithSimanPro: React.ComponentType
+}
+
+const WrapperAddToCart: FunctionComponent<WrapperAddToCartProps> = ({ AddToCartWithSimanPro, AddToCartWithoutSimanPro, ProductQuantity }) => {
+  const product = useProduct()
+
+  if(!product) return <></>
+
+  const isSimanProProduct = product?.product?.specificationGroups?.find((specification) => specification.name === 'Siman Pro')
+
+  if(isSimanProProduct) return <AddToCartWithSimanPro />
+  
+  return (
+    <>
+      <ProductQuantity />
+      <AddToCartWithoutSimanPro/>
+    </>
+  )
+}
+
+export default WrapperAddToCart 
