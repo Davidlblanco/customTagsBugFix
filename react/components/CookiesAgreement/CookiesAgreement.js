95f5aaef1dd9b829b5184effbf1df041488c9e46
Geordy  Reyes <geordymemdoza@gmail.com>
2022-09-19 17:49:04 -0600

    Fix: validation

--- react/components/CookiesAgreement/CookiesAgreement.js
+++ react/components/CookiesAgreement/CookiesAgreement.js
@@ -3,31 +3,26 @@ import "./Cookies.css";
 
 
 function CookiesAgreement() {
-    var myClass = 'vtex-flex-layout-0-x-flexRow--cookies_modal';
-    var storeValue = window.localStorage.getItem('value')
+  var myClass = 'vtex-flex-layout-0-x-flexRow--cookies_modal';
 
-    storeValue == 'true' ? myClass = 'vtex-flex-layout-0-x-flexRow--cookies_modalOff' : myClass = 'vtex-flex-layout-0-x-flexRow--cookies_modal'
+  function savePreference() {
+    localStorage.setItem('cookies', 'true');
+    document.getElementsByClassName(myClass)[0].style.display = 'none';
+  }
 
-    function savePreference() {
-        console.log("AHHHHHHHHH")
-        if (window.localStorage.getItem('value') == null) {
-            window.localStorage.setItem('value', true);
-            myClass = 'vtex-flex-layout-0-x-flexRow--cookies_modalOff';
-            console.log(storeValue)
-        };
-    };
-
-
+  if (localStorage.getItem('cookies') === 'true') {
+    return null;
+  } else {
     return (
-        <div className={myClass} >
-            <div>
-                <h2>Este sitio web utiliza cookies</h2>
-                <p>Esta web utiliza cookies propias y de terceros para ofrecerle una mejor experiencia y servicio. Al navegar o utilizar nuestros servicios acepta el uso que hacemos de las cookies.</p>
-                <button className='vtex-flex-layout-0-x-flexRow--cookies_button' onClick={savePreference}>Aceptar</button>
-            </div>
+      <div className={myClass} id="cookiesAgreement">
+        <div>
+          <h2>Este sitio web utiliza cookies</h2>
+          <p>Esta web utiliza cookies propias y de terceros para ofrecerle una mejor experiencia y servicio. Al navegar o utilizar nuestros servicios acepta el uso que hacemos de las cookies.</p>
+          <button className='vtex-flex-layout-0-x-flexRow--cookies_button' onClick={savePreference}>Aceptar</button>
         </div>
-    )
-
+      </div>
+    );
+  }
 }
 
 export default CookiesAgreement
