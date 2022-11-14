eb3a56e38508fe7bd17d513c7310a5e6d7aa4947
Geordy  Reyes <geordymemdoza@gmail.com>
2022-11-14 09:35:01 -0600

    Fix: live test

--- react/components/FiraLive/index.tsx
+++ react/components/FiraLive/index.tsx
@@ -5,19 +5,11 @@ import styles from "./styles.css";
 const FiraSiman = () => {
     return (
       <div className={styles.firaContainer}>
-
-        <div id='fira-siman'></div>
-        
-        <script src="https://firalivepro.blob.core.windows.net/fira-live-widget/index.min.js" id="fira-live-widget" fira-key="6362f88266e0136a7056f59e"></script>
-
-        <script>setupIframePlayerWithLatestLive("fira-siman");</script>
+<iframe  src='https://promocionessiman.com/landing/fira-live-test.html'></iframe>
 
       </div>
-/*       <div className={styles.firaContainer}>
-        iheyrgblieahrigeau
-      </div> */
-    )
 
+    )
 }
 
 export {FiraSiman}
