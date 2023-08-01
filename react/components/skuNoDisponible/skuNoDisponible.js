447a2ebdf35a0233e07b522789e54087f65d65ff
marvinNlasco <marvin_nolasco@siman.com>
2023-08-01 15:13:13 -0600

    Fix sku variantes ordenadas por stock

--- react/components/skuNoDisponible/skuNoDisponible.js
+++ react/components/skuNoDisponible/skuNoDisponible.js
@@ -4,33 +4,31 @@ import React, { useEffect } from "react";
 const skuNoDisponible = () => {
     useEffect(
         () => {
-            const container = document.getElementsByClassName('vtex-store-components-3-x-skuSelectorOptionsList') ?? false;
-            // I not find a sku selector
-            if (!container) {
-                return false;
+            const container = document.getElementsByClassName('vtex-store-components-3-x-skuSelectorOptionsList') ?? null;
+
+            if (container.length > 0) {
+                //Map the children
+                Array.from(container[0]?.children).map((child, index) => {
+                    // Evaluation if its available
+                    const isUnavailable = child.querySelector('.vtex-store-components-3-x-valueWrapper--unavailable');
+
+                    if (child.classList.contains("vtex-store-components-3-x-seeMoreButton")) {
+                        child.style.order = container[0].children.length + 1;
+                    }
+
+                    if (isUnavailable != null) {
+                        // Add a order to the unavailable items
+                        child.style.order = index + 1;
+                    }
+                });
             }
-
-            //Map the children
-            Array.from(container[0].children).map((child, index) => {
-                // Evaluation if its available
-                const isUnavailable = child.querySelector('.vtex-store-components-3-x-valueWrapper--unavailable');
-
-                if (child.classList.contains("vtex-store-components-3-x-seeMoreButton")) {
-                    child.style.order = container[0].children.length + 1;
-                }
-
-                if (isUnavailable != null) {
-                    // Add a order to the unavailable items
-                    child.style.order = index + 1;
-                }
-            });
             //Add classes to modify the container behavior in order to have the unavailable items at the beginning
             //container[0].style.flexFlow = 'row-reverse wrap-reverse';
             // container[0].style.justifyContent = 'flex-end';
         }, []
     )
     return (
-        <div></div>
+        <></>
     )
 }
 export default skuNoDisponible;
