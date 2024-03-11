c22892993e76a34d9fc78b83a0005a5c609da059
Savio <saviochwt@gmail.com>
2024-03-11 16:27:08 -0300

    feat: new styles and icons to 360Photos

---
+++ react/components/360Photos/Icons/CloseModal360.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Close = () => {
+    return (
+        <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.952 -9.53674e-07L6.976 5.728L10.08 12H7.248L5.712 8.544L5.216 7.344H5.168L4.672 8.544L3.168 12H0.288L3.328 5.792L0.352 -9.53674e-07H3.296L4.672 3.024L5.184 4.208H5.232L5.728 3.024L7.104 -9.53674e-07H9.952Z" fill="black" />
+        </svg>
+    )
+}
+
+export default Close;
