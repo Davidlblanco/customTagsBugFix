7afd2d9df66d6154760c75a0586c14e36ff4dd08
Savio <saviochwt@gmail.com>
2024-03-20 13:26:08 -0300

    hotfix: wrapper add to cart

--- react/components/WrapperAddToCartNew/index.tsx
+++ react/components/WrapperAddToCartNew/index.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent } from "react";
 import { useProduct } from "vtex.product-context";
 
 interface WrapperAddToCartProps {
-    ProductQuantity: React.ComponentType;
     AddToCartWithoutSimanPro: React.ComponentType;
     AddToCartWithSimanPro: React.ComponentType;
 }
@@ -10,25 +9,16 @@ interface WrapperAddToCartProps {
 const WrapperAddToCart: FunctionComponent<WrapperAddToCartProps> = ({
     AddToCartWithSimanPro,
     AddToCartWithoutSimanPro,
-    ProductQuantity,
 }) => {
     const product = useProduct();
 
     if (!product) return <></>;
 
-    const isSimanProProduct = product?.product?.specificationGroups?.find(
-        (specification) =>
-            specification.originalName.toLowerCase() === "siman pro"
-    );
+  const isSimanProProduct = product?.product?.specificationGroups?.find((specification) => specification.originalName.toLowerCase() === 'siman pro')
 
     if (isSimanProProduct) return <AddToCartWithSimanPro />;
 
-    return (
-        <>
-            <ProductQuantity />
-            <AddToCartWithoutSimanPro />
-        </>
-    );
+    return <AddToCartWithoutSimanPro />;
 };
 
 export default WrapperAddToCart;
