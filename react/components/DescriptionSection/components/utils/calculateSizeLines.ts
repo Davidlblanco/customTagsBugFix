246d4f118e7dff74c350463944aee5a65f59bcfa
Savio <saviochwt@gmail.com>
2024-03-20 15:22:40 -0300

    fix: avoid error on CalculateSizeLines

--- react/components/DescriptionSection/components/utils/calculateSizeLines.ts
+++ react/components/DescriptionSection/components/utils/calculateSizeLines.ts
@@ -1,5 +1,5 @@
 export const calculateSizeLines = (text: string) => {
-    var numLines = Math.ceil(text.length / 97);
+    var numLines = Math.ceil(text?.length / 97);
     var lineSize = numLines * 24;
     return lineSize;
 }
