36cdd37b8973862aa20f7bab063ddb3d152ac74b
Savio <saviochwt@gmail.com>
2024-03-20 16:41:11 -0300

    fix: Product Comparator Specifications

--- react/components/ProductComparator/components/ProductComparatorSpecifications/Specification/Specification.tsx
+++ react/components/ProductComparator/components/ProductComparatorSpecifications/Specification/Specification.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import { useProduct } from "vtex.product-context";
 
 import SpecificationValues from "./SpecificationValues/SpecificationValues";
-import { Item } from '../../../types/ProductComparator';
+import { Item } from "../../../types/ProductComparator";
 
 import { reorganizeProperties } from "../../../utils/reorganizeProperties";
 import { filterItems } from "../../../utils/filterItems";
 
-import styles from './styles.css';
+import styles from "./styles.css";
 
 interface SpecificationProps {
     items: Item[][];
@@ -16,7 +16,14 @@ interface SpecificationProps {
 
 const Specification = ({ items }: SpecificationProps) => {
     const productContext = useProduct();
-    const propertiesProductSeen = productContext?.product?.properties;
+
+    const specificationGroups =
+        productContext?.product?.specificationGroups ?? [];
+
+    const propertiesProductSeen = specificationGroups.find(
+        (spec) => spec.originalName == "allSpecifications"
+    )?.specifications;
+
     const hasItems = items && items.length > 0;
 
     if (!hasItems || !propertiesProductSeen) {
@@ -30,12 +37,16 @@ const Specification = ({ items }: SpecificationProps) => {
         <div className={styles["container-specification"]}>
             <ul className={styles["wrap-list"]}>
                 {Object.entries(propertiesValues)?.map(([name, values]) => {
-                    const propertySeen = propertiesProductSeen.find(item => item.name === name);
+                    const propertySeen = propertiesProductSeen.find(
+                        (item) => item.name === name
+                    );
                     return (
                         <li className={styles["list-item"]} key={name}>
                             <div className={styles["item-name"]}>
                                 <span>{name}</span>
-                                {propertySeen && <span>{propertySeen.values[0]}</span>}
+                                {propertySeen && (
+                                    <span>{propertySeen.values[0]}</span>
+                                )}
                             </div>
                             <SpecificationValues values={values} />
                         </li>
