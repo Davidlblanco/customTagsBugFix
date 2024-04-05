deb5658d42b9d319c3511a1e6ebd96261640b62e
Leonardo Costa <leonardo.costa.b@outlook.com>
2024-04-05 11:32:13 -0300

    feat: cache created for credisiman installments

--- react/components/CredisimanCuotas/index.tsx
+++ react/components/CredisimanCuotas/index.tsx
@@ -1,39 +1,19 @@
 import React from "react";
-import useProductPayments from "../Cuotas/hooks/useProductPayments";
+import { handleCredisimanInstallments } from "./utils/handleCredisimanInstallments";
 
 import style from './styles.modules.css';
 
 export function CredisimanCuotas() {
-  console.log('renderizou')
-
-  const { isLoading, bestInstallment } = useProductPayments({
-    paymentIds: ["401","403"], // This filter is optional
-  });
-
-  if (isLoading) {
-    return (
-      <div className="">
-        Carregando...
-      </div>
-    );
-  }
+  const { credisiman } = handleCredisimanInstallments();
 
   const canRender =
-      bestInstallment?.installment && bestInstallment.installment > 1;
+  credisiman?.installment && credisiman.installment > 1;
 
-    console.log({
-      bestInstallment,
-      isLoading,
-      canRender,
-    })
-
-  if (!canRender) {
-      return <></>;
-  }
+  if (!canRender) return <></>
 
   return (
     <div className={style.credisimanContainer}>
-      <span className={style.installments}>{bestInstallment?.installment} Cuotas</span>
+      <span className={style.installments}>{credisiman?.installment} Cuotas</span>
       <img 
         src="/arquivos/credisiman-tag-pdp.png" 
         alt="tarjeta de crédito siman en color azul" 
