229b6fc0e5b5cec75590c1150465f67debc7238f
Pedro Felipe <felipexexeu02@gmail.com>
2024-06-26 11:49:15 -0300

    feat(TagsCoutas): adjust styles tags quotas on mobile

--- react/components/TagsCoutas/TagsCoutas.tsx
+++ react/components/TagsCoutas/TagsCoutas.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import useProductPayments from "../Cuotas/hooks/useProductPayments";
 import { formatTags } from "./utils/formatTags";
 
+import style from './styles.css'
+
 const TagsCoutas = () => {
     const { bestPayment } = useProductPayments({
         paymentIds: [],
@@ -10,30 +12,26 @@ const TagsCoutas = () => {
     const formattedTags = formatTags(bestPayment?.tagsCuotas);
 
     return (
-        <div
-            className="flex items-center flex-wrap pb3"
-            style={{ gap: "6px" }}
-        >
+        <div className={style.containerTagsCuotas}>
             {formattedTags &&
                 formattedTags?.map(({ tag, tagStyle }, index) => (
                     <div
                         key={index}
-                        className="flex items-center"
-                        style={{ gap: "6px" }}
+                        className={style.tagsCuotas}
                     >
                         {tag?.tagText && (
-                            <div style={tagStyle}>
+                            <div
+                                className={style.tagStyle}
+                                style={tagStyle}
+                            >
                                 {tag.tagText}
                             </div>
                         )}
                         {tag?.tagImage?.url && (
                             <img
                                 src={tag.tagImage.url}
-                                alt="Tag"
-                                style={{
-                                    maxWidth: "20px",
-                                    maxHeight: "20px"
-                                }}
+                                alt={'Tag cuotas'}
+                                className={style.tagImg}
                             />
                         )}
                     </div>
--- react/components/TagsCoutas/utils/formatTags.ts
+++ react/components/TagsCoutas/utils/formatTags.ts
@@ -1,16 +1,19 @@
 import { TagCuotasValues } from "../../Cuotas/Types/PaymentCustom";
+import { CSSProperties } from 'react';
 
 export const formatTags = (tags: TagCuotasValues[] | undefined | null) => {
   return tags?.map((value) => {
     const { tag } = value;
-    const tagStyle = {
+    const tagStyle: CSSProperties = {
       ...tag?.tagDesign,
       borderStyle: "solid",
       borderWidth: "1px",
-      padding: "5px 9px",
+      padding: "5px 6px",
       lineHeight: "initial",
       fontWeight: 700,
       fontFamily: "Roboto",
+      minWidth: "68px",
+      textAlign: "center"
     };
     return { tag, tagStyle };
   });
