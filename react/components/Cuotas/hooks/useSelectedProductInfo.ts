a9973abe86d7e2fb4d026505dc83e3fd9c4a4d9d
Leonardo R <leonardo.souza@cadastra.com>
2024-07-23 11:28:47 -0300

    fix: fix errors

--- react/components/Cuotas/hooks/useSelectedProductInfo.ts
+++ react/components/Cuotas/hooks/useSelectedProductInfo.ts
@@ -3,31 +3,36 @@ import UseSimanPro from "./useSimanPro";
 import useProductSearch from "../../../hooks/useProductSearch";
 
 export default function useSelectedProductInfo() {
-   const productContext = useProduct();
-   const simanpro = UseSimanPro();
+    const productContext = useProduct();
+    const simanpro = UseSimanPro();
 
-   const productItem = productContext?.product;
-   const selectedItem = productContext?.selectedItem;
+    const productItem = productContext?.product;
+    const selectedItem = productContext?.selectedItem;
 
-   const productSearch = useProductSearch({ IDs: selectedItem?.itemId ? [selectedItem?.itemId] : [""] })
+    const productSearch = useProductSearch({
+        IDs: selectedItem?.itemId ? [selectedItem?.itemId] : undefined,
+    });
 
-   const selectedItemPrice = selectedItem?.sellers[0]?.commertialOffer?.Price ?? 0;
-   const selectedQuantity = productContext?.selectedQuantity ?? 0;
-   const productQuantityPrice = selectedItemPrice * selectedQuantity * 100;
+    const selectedItemPrice =
+        selectedItem?.sellers[0]?.commertialOffer?.Price ?? 0;
+    const selectedQuantity = productContext?.selectedQuantity ?? 0;
+    const productQuantityPrice = selectedItemPrice * selectedQuantity * 100;
 
-   return {
-      skuId: selectedItem?.itemId ?? "",
-      totalPrice: productQuantityPrice + simanpro.total,
-      sellerId: selectedItem?.sellers[0].sellerId ?? "",
-      categoriesIds: productSearch[0]?.categoryTree?.map((category) => category.id) ?? [
-         productItem?.categoryId ? String(productItem?.categoryId) : "",
-      ],
-      brandId: productItem?.brandId ? productItem.brandId.toString() : "",
-      simanpro: simanpro,
-      productClusters: productSearch[0]?.productClusters ? productSearch[0]?.productClusters?.map((item) => {
-         return item?.id
-      }) : []
-   };
+    return {
+        skuId: selectedItem?.itemId ?? "",
+        totalPrice: productQuantityPrice + simanpro.total,
+        sellerId: selectedItem?.sellers[0].sellerId ?? "",
+        categoriesIds: productSearch[0]?.categoryTree?.map(
+            (category) => category.id
+        ) ?? [productItem?.categoryId ? String(productItem?.categoryId) : ""],
+        brandId: productItem?.brandId ? productItem.brandId.toString() : "",
+        simanpro: simanpro,
+        productClusters: productSearch[0]?.productClusters
+            ? productSearch[0]?.productClusters?.map((item) => {
+                  return item?.id;
+              })
+            : [],
+    };
 }
 
 export type SelectedProductInfo = ReturnType<typeof useSelectedProductInfo>;
