2417feb7355ac313140bfba549d4b0657fe0cb65
Erick Dahl <erickdahl1@gmail.com>
2024-07-24 10:23:22 -0300

    fix: css render order, function name

--- react/components/TagsCuotas/index.tsx
+++ react/components/TagsCuotas/index.tsx
@@ -4,7 +4,7 @@ import formatTags from "./utils/formatTags";
 
 import style from "./styles.css";
 
-const TagsCoutas = () => {
+const TagsCuotas = () => {
     const { results } = useProductPayments({
         paymentIds: [],
     });
@@ -26,25 +26,24 @@ const TagsCoutas = () => {
 
     return (
         <div className={style.containerTagsCuotas}>
-            {formattedTags &&
-                formattedTags?.map(({ tag, tagStyle }, index) => (
-                    <div key={index} className={style.tagsCuotas}>
-                        {tag?.tagText && (
-                            <div className={style.tagStyle} style={tagStyle}>
-                                {tag.tagText}
-                            </div>
-                        )}
-                        {tag?.tagImage?.url && (
-                            <img
-                                src={tag.tagImage.url}
-                                alt={"Tag cuotas"}
-                                className={style.tagImg}
-                            />
-                        )}
-                    </div>
-                ))}
+            {formattedTags?.map(({ tag, tagStyle }, index) => (
+                <div key={index} className={style.tagsCuotas}>
+                    {tag?.tagText && (
+                        <div className={style.tagStyle} style={tagStyle}>
+                            {tag.tagText}
+                        </div>
+                    )}
+                    {tag?.tagImage?.url && (
+                        <img
+                            src={tag.tagImage.url}
+                            alt="Tag cuotas"
+                            className={style.tagImg}
+                        />
+                    )}
+                </div>
+            ))}
         </div>
     );
 };
 
-export default TagsCoutas;
+export default TagsCuotas;
