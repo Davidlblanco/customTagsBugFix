28b5c183c378814e46e22e2ccecd921fd82931d3
Leonardo Costa <leonardo.costa.b@outlook.com>
2024-07-26 10:06:36 -0300

    fix: position text

--- react/components/CrediSimanPrice/index.tsx
+++ react/components/CrediSimanPrice/index.tsx
@@ -4,18 +4,19 @@ import { useProduct } from "vtex.product-context";
 import { ConfigGroup, CredisimanType } from "./Types/credisimanTypes";
 import { GetCrediSimanProductData } from "./Logic/logic";
 // import CrediSimanImage from "../../utils/CredisimanImage/CredisimanImage";
-import styles from "./CrediSimanPrice.css";
-// import GetPageType from "../../utils/getPageType";
+import GetPageType from "../../utils/getPageType";
 import { useRenderSession } from "vtex.session-client";
 import { handleCredisimanStyles } from "./utils/handleCredisimanStyles";
+import styles from "./CrediSimanPrice.css";
 
 interface CredisimanPriceProps {
   isShelf?: boolean;
 }
 
-const CrediSimanPrice: StorefrontFunctionComponent<CredisimanPriceProps> = ({ children, isShelf }) => {
+const CrediSimanPrice: StorefrontFunctionComponent<CredisimanPriceProps> = ({ isShelf }) => {
     const productContext = useProduct();
-    // const pageType = GetPageType();
+
+    const pageType = GetPageType();
     const skuId = productContext?.selectedItem?.itemId;
     const productId = productContext?.product?.productId;
 
@@ -25,12 +26,6 @@ const CrediSimanPrice: StorefrontFunctionComponent<CredisimanPriceProps> = ({ ch
     const { session } = useRenderSession();
 
     const sallesChannelId = session?.namespaces?.store?.channel?.value;
-    // const countryAccount: string =
-    //     session?.namespaces?.account?.accountName?.value ?? "siman";
-
-    // const IsProductPage = (): boolean => {
-    //     return pageType === "product";
-    // };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -55,118 +50,80 @@ const CrediSimanPrice: StorefrontFunctionComponent<CredisimanPriceProps> = ({ ch
         fetchData();
 
     }, [productId, skuId, sallesChannelId]);
-
-    // if (!children) return <></>
     
-    if (!productData) return (
-      <div className={`${styles['tag-preview__price']} ${styles['no-promotion']}`}>
-        {children}
-      </div>
-    );
-
-
-    //const shouldShowTag = productData?.discountValue > 0;
+    if (!productData) return <></>
 
     return (
-          <div>
-            <div className={styles['tag-preview__credisiman']}>
-              {credisimanTagStyles?.viewFields.price.active && (
-                <span
-                  className={styles['tag-preview__credisiman-price']}
-                  style={{
-                    color: credisimanTagStyles?.viewFields.price.color,
-                    fontSize: `${credisimanTagStyles?.tagStyles.fontSize}px`,
-                    order: credisimanTagStyles?.image.position === 'right' ? 1 : 2,
-                  }}
-                >
-                  <FormattedCurrency value={productData?.totalWithDiscount} />
-                </span>
-              )}
-
-              {credisimanTagStyles?.viewFields.porcentage && (
-                <span
-                  className={styles['tag-preview__credisiman-porcentage']}
-                  style={{
-                    borderRadius: `${credisimanTagStyles?.tagStyles.borderRadius}px`,
-                    borderColor: credisimanTagStyles?.tagStyles.borderColor,
-                    color: credisimanTagStyles?.tagStyles.color,
-                    backgroundColor: credisimanTagStyles?.tagStyles.backgroundColor,
-                    order: credisimanTagStyles?.image.position === 'right' ? 2 : 3,
-                  }}
-                >
-                  {productData?.discountValue}%
-                </span>
-              )}
-
-              {credisimanTagStyles?.viewFields.img && (
-                <img
-                  src={
-                    credisimanTagStyles?.image.src.length
-                      ? credisimanTagStyles?.image.src
-                      : 'https://simanqa.vtexassets.com/assets/simanqa.file-manager/images/visa-tag___bbbaf9ca29725b058b26414be9398435.svg'
-                  }
-                  alt="Credisiman Tag"
-                  width={24}
-                  height={24}
-                  style={{
-                    order: credisimanTagStyles?.image.position === 'right' ? 3 : 1,
-                  }}
-                />
-              )}
-            </div>
-
-            <div
-              className={`${styles['tag-preview__price-container']} ${
-                isShelf ? styles.shelf : ''
-              }`}
+      <div
+        className={`
+          ${styles['tag-preview__credisiman-container']} 
+          ${isShelf ? styles.shelf : ''}
+          ${pageType === 'product' && !isShelf ? styles.pdp : ''}
+        `}
+      >
+        <div
+          className={styles['tag-preview__credisiman']}
+          style={{
+            order: credisimanTagStyles?.viewFields.text.position === 'right' ? 1 : 2,
+          }}
+        >
+          {credisimanTagStyles?.viewFields.price.active && (
+            <span
+              className={styles['tag-preview__credisiman-price']}
+              style={{
+                color: credisimanTagStyles?.viewFields.price.color,
+                fontSize: `${credisimanTagStyles?.tagStyles.fontSize}px`,
+                order: credisimanTagStyles?.image.position === 'right' ? 1 : 2,
+              }}
             >
-              <div
-                className={styles['tag-preview__price']}
-                style={{
-                  order: credisimanTagStyles?.viewFields.text.position === 'right' ? 1 : 2,
-                }}
-              >
-                {children}
-              </div>
+              <FormattedCurrency value={productData?.totalWithDiscount} />
+            </span>
+          )}
 
-              {credisimanTagStyles?.viewFields.text.active && (
-                <p
-                  className={styles['tag-preview__info-text']}
-                  style={{
-                    color: credisimanTagStyles?.viewFields.text.color,
-                    order: credisimanTagStyles?.viewFields.text.position === 'right' ? 2 : 1,
-                  }}
-                >
-                  {credisimanTagStyles?.viewFields.text.phrase}
-                </p>
-              )}
-            </div>
-          </div>
+          {credisimanTagStyles?.viewFields.porcentage && (
+            <span
+              className={styles['tag-preview__credisiman-porcentage']}
+              style={{
+                borderRadius: `${credisimanTagStyles?.tagStyles.borderRadius}px`,
+                borderColor: credisimanTagStyles?.tagStyles.borderColor,
+                color: credisimanTagStyles?.tagStyles.color,
+                backgroundColor: credisimanTagStyles?.tagStyles.backgroundColor,
+                order: credisimanTagStyles?.image.position === 'right' ? 2 : 3,
+              }}
+            >
+              {productData?.discountValue}%
+            </span>
+          )}
 
-          /* <div
-              className={
-                  IsProductPage()
-                      ? styles.customCrediSimanContainer
-                      : styles.customCrediSimanContainerCategory
+          {credisimanTagStyles?.viewFields.img && (
+            <img
+              src={
+                credisimanTagStyles?.image.src.length
+                  ? credisimanTagStyles?.image.src
+                  : 'https://simanqa.vtexassets.com/assets/simanqa.file-manager/images/visa-tag___bbbaf9ca29725b058b26414be9398435.svg'
               }
+              alt="Credisiman Tag"
+              width={24}
+              height={24}
+              style={{
+                order: credisimanTagStyles?.image.position === 'right' ? 3 : 1,
+              }}
+            />
+          )}
+        </div>
+
+        {credisimanTagStyles?.viewFields.text.active && (
+          <p
+            className={styles['tag-preview__info-text']}
+            style={{
+              color: credisimanTagStyles?.viewFields.text.color,
+              order: credisimanTagStyles?.viewFields.text.position === 'right' ? 2 : 1,
+            }}
           >
-              <span
-                  className={
-                      IsProductPage()
-                          ? styles.customCrediSimanPriceProduct
-                          : styles.customCredisimanPriceCategory
-                  }
-              >
-                  <FormattedCurrency value={productData?.totalWithDiscount} />
-              </span>
-              {/* shouldShowTag && (
-                  <div className={styles.customCrediSimanTag}>
-                      <span className={styles.customCrediSimanTagText}>
-                          -{productData?.discountValue}%
-                      </span>
-                  </div>
-              )
-          </div> */
+            {credisimanTagStyles?.viewFields.text.phrase}
+          </p>
+        )}
+      </div>
     );
 };
 
