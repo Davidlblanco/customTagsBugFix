4731028ff4085973a58d864ae23472cb36a32674
Leonardo R <leonardo.souza@cadastra.com>
2024-07-30 10:59:50 -0300

    fix: unify installment tag in order to fix line breake

--- react/components/TagsCuotas/index.tsx
+++ react/components/TagsCuotas/index.tsx
@@ -1,67 +1,13 @@
 import React from "react";
-import useProductPayments from "../Cuotas/hooks/useProductPayments";
-import formatTags from "./utils/formatTags";
-
 import style from "./styles.css";
-import { TagCuotasValues } from "../Cuotas/Types/PaymentCustom";
+import TagCuotasListItems from "./TagCuotasListItems";
 
 const TagsCuotas = () => {
-    const { results } = useProductPayments({
-        paymentIds: [],
-    });
-
-    const allValidTags = results
-        .filter((payment) => payment.isValid)
-        .flatMap((payment) => filterHighestBankTag(payment.tagsCuotas))
-        .filter(
-            (tagsCuotas) => tagsCuotas !== null && tagsCuotas !== undefined
-        );
-
-    const formattedTags = formatTags(allValidTags);
-
     return (
         <div className={style.containerTagsCuotas}>
-            {formattedTags?.map(({ tag, tagStyle }, index) => (
-                <div key={index} className={style.tagsCuotas}>
-                    {tag?.tagText && (
-                        <div className={style.tagStyle} style={tagStyle}>
-                            {tag.tagText}
-                        </div>
-                    )}
-                    {tag?.tagImage?.url && (
-                        <img
-                            src={tag.tagImage.url}
-                            alt="Tag cuotas"
-                            className={style.tagImg}
-                        />
-                    )}
-                </div>
-            ))}
+            <TagCuotasListItems />
         </div>
     );
 };
-
-function filterHighestBankTag(data?: TagCuotasValues[] | null) {
-    if (!data) return [];
-    const resp: TagCuotasValues[] = [];
-
-    for (const item of data) {
-        const bankId = item.bank.id;
-        const itemInArrayIndex = resp.findIndex((x) => x.bank.id === bankId);
-
-        if (itemInArrayIndex === -1) {
-            resp.push(item);
-            continue;
-        }
-
-        const months = item.months.value;
-        const monthsInArray = resp[itemInArrayIndex].months.value;
-        if (months > monthsInArray) {
-            resp[itemInArrayIndex] = item;
-        }
-    }
-
-    return resp;
-}
 
 export default TagsCuotas;
