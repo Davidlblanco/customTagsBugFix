e212696c5cea392fd2cb252b38f475fc7bce40d0
Pedro Felipe <felipexexeu02@gmail.com>
2024-08-08 10:53:43 -0300

    fix(CuotasProductSummary): remove best Installment Value check

--- react/components/Cuotas/components/CuotasProductSummary/CuotasProductSummary.tsx
+++ react/components/Cuotas/components/CuotasProductSummary/CuotasProductSummary.tsx
@@ -4,8 +4,6 @@ import InstallmentDetails from "../InstallmentDetail/InstallmentDetails";
 import PaymentImages from "../PaymentImages/PaymentImages";
 import { handleTags } from "../CuotasPdp/utils/handleTags";
 
-import { bestInstallmentValues } from "../CuotasPdp/utils/bestInstallmentValues";
-
 import { GenericTagsFront } from "../../Types/PaymentCustom";
 import { BestInstallment } from "../../Types/BestInstallment";
 import { Results } from "../../Types/Results";
@@ -24,13 +22,12 @@ const CuotasProductSummary = ({
     results
 }: CuotasProductSummaryProps) => {
     const { updateAllTagsPreview } = handleTags(results, tagsPreview);
-    const installmentValues = bestInstallmentValues(bestInstallment, results);
     return (
         <div className={`${styles.cuotasProductSummary}`}>
             {updateAllTagsPreview && updateAllTagsPreview.tagIsActive && (
                 <div className={`${styles.tagPreviewWrapper}`}>
                     <InstallmentDetails
-                        installment={installmentValues?.installment}
+                        installment={bestInstallment?.installment}
                         tag={{
                             quantityImgs: updateAllTagsPreview?.tagsImgs?.length,
                             styles: updateAllTagsPreview.styles,
