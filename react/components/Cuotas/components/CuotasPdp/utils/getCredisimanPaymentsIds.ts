2809797bfb1a3c9140234f0cf1d204a08bda1056
Savio <saviochwt@gmail.com>
2024-10-01 11:10:31 -0300

    feat(CuotasPdp): add new Credisiman payment ID (interest)

--- react/components/Cuotas/components/CuotasPdp/utils/getCredisimanPaymentsIds.ts
+++ react/components/Cuotas/components/CuotasPdp/utils/getCredisimanPaymentsIds.ts
@@ -3,7 +3,7 @@ import { useRuntime } from "vtex.render-runtime";
 export const getCredisimanPaymentsIds = (): string[] => {
     const { account } = useRuntime();
     const countryAccount: string = account ?? "siman";
-    const prodCredisimanIDs = ["401", "404"];
+    const prodCredisimanIDs = ["401", "404", "406"];
     const qaCredisimanIDs = ["402", "403", "406"];
 
     return countryAccount.includes("qa") ? qaCredisimanIDs : prodCredisimanIDs;
