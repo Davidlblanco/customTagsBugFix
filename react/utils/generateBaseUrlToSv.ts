f406d1a4655d5b32b02f4f119db24bc6fd5433a4
Savio <saviochwt@gmail.com>
2024-10-01 16:18:15 -0300

    fix: Refactor generateBaseUrlToSv function to remove unnecessary "hu056--" prefix for non-SV accounts

--- react/utils/generateBaseUrlToSv.ts
+++ react/utils/generateBaseUrlToSv.ts
@@ -2,7 +2,7 @@ export default function generateBaseUrlToSV(account: string, workspace: string) 
     let host = `${workspace ?? ""}--${account}`;
     const isSVAccount = account == "siman" || account == "simanqa";
   
-    if (!isSVAccount) host = account.includes("qa") ? "hu056--simanqa" : "hu056--siman"; //TODO remover "hu056--"
+    if (!isSVAccount) host = account.includes("qa") ? "simanqa" : "siman";
   
     return `https://${host}.myvtex.com`;
   }
