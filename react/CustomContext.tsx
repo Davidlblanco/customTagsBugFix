14792ac07add7be770884d510bf896f101851243
Savio <saviochwt@gmail.com>
2024-10-03 17:49:46 -0300

    fix: Update CustomContext to include CustomTagConfigsContextProvider

--- react/CustomContext.tsx
+++ react/CustomContext.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { AvailablePaymentConfigsContextProvider } from "./contexts/AvailablePaymentConfigsContext";
 import { GenericTagsContextProvider } from "./contexts/GenericTagsContext";
+import { CustomTagConfigsContextProvider } from "siman.custom-tags";
 
 export default function CustomContext({ children }) {
     return (
-      <AvailablePaymentConfigsContextProvider>
-        <GenericTagsContextProvider>
-            {children}
-        </GenericTagsContextProvider>
-      </AvailablePaymentConfigsContextProvider>
+        <CustomTagConfigsContextProvider>
+            <AvailablePaymentConfigsContextProvider>
+                <GenericTagsContextProvider>
+                    {children}
+                </GenericTagsContextProvider>
+            </AvailablePaymentConfigsContextProvider>
+        </CustomTagConfigsContextProvider>
     );
 }
