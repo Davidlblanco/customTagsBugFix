3f0de12133480d212c51a263398c9e4badddd210
Pedro Felipe <felipexexeu02@gmail.com>
2024-10-03 11:08:46 -0300

    feat(CuotasProductSummary): remove PaymentImages from CuotasProductSummary

--- react/components/Cuotas/components/CuotasProductSummary/CuotasProductSummary.tsx
+++ react/components/Cuotas/components/CuotasProductSummary/CuotasProductSummary.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import InstallmentDetails from "../InstallmentDetail/InstallmentDetails";
-import PaymentImages from "../PaymentImages/PaymentImages";
 import { handleTags } from "../CuotasPdp/utils/handleTags";
 
 import { GenericTagsFront } from "../../Types/PaymentCustom";
@@ -18,8 +17,6 @@ interface CuotasProductSummaryProps {
 
 const CuotasProductSummary = ({ tagsPreview, bestInstallment, results }: CuotasProductSummaryProps) => {
     const { updateAllTagsPreview } = handleTags(results, tagsPreview);
-    const verifyTagsPreview =
-        updateAllTagsPreview && updateAllTagsPreview?.tagIsActive && updateAllTagsPreview?.tagsImgs?.length > 0;
     return (
         <div className={`${styles.cuotasProductSummary}`}>
             {results.length > 0 && (
@@ -33,17 +30,6 @@ const CuotasProductSummary = ({ tagsPreview, bestInstallment, results }: CuotasP
                         }}
                         visibility={"product-summary"}
                     />
-                    {verifyTagsPreview && (
-                        <PaymentImages
-                            paymentsImages={updateAllTagsPreview?.tagsImgs}
-                            availablePayments={results?.map((result) => ({
-                                paymentId: result.paymentId,
-                                isValid: result.isValid,
-                                BankTypes: result?.BankTypes
-                            }))}
-                            tagStyles={updateAllTagsPreview?.styles}
-                        />
-                    )}
                 </div>
             )}
         </div>
