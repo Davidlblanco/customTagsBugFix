628f767d1a9a2ccfb14fd9a07dc1193b663fc43f
Walter Castillo - Senior WebDev <walter_castillo@siman.com>
2024-10-16 11:37:40 -0600

    fix: change classes name to prevent conflicts

--- react/components/ProductIngredients/ProductIngredients.tsx
+++ react/components/ProductIngredients/ProductIngredients.tsx
@@ -14,29 +14,29 @@ const ProductIngredients = () => {
     const ingredients = allSpecifications?.specifications.find(
         (specification) => specification.name === "Ingredientes"
     );
-    const ingredients_list = "• " + ingredients?.values[0].replace(/,/g, " • ").split(", ") || "";
+    const ingredients_list = "• " + ingredients?.values[0]?.replace(/,/g, " • ").split(", ") || "";
 
     if (ingredients_list.length <= 0 || !categories.includes('/Belleza e higiene/')) return <></>;
 
     return (
-        <div className={styles.componentContainer}>
-            <span className={styles.title}>Ingredientes</span> <br />
-            <span className={styles.specificationValue}>
+        <div className={styles.componentContainerIngredients}>
+            <span className={styles.titleIngredients}>Ingredientes</span> <br />
+            <span className={styles.specificationValueIngredients}>
                 {ingredients_list}
             </span>
             {ingredients_list?.length > 100 && (
                 <button
-                    className={styles.showMore}
+                    className={styles.showMoreIngredients}
                     onClick={() => setShowAll(!showAll)}
                 >Ver más</button>
             )}
             {showAll && (
-                <div className={styles.modal}>
-                    <div className={styles.modalContent}>
-                        <button className={styles.close} onClick={() => setShowAll(!showAll)}>x</button>
-                        <p className={styles.titleModal}>Ingredientes</p> <br />
+                <div className={styles.modalIngredients}>
+                    <div className={styles.modalContentIngredients}>
+                        <button className={styles.closeIngredients} onClick={() => setShowAll(!showAll)}>x</button>
+                        <p className={styles.titleModalIngredients}>Ingredientes</p> <br />
                         <div className={styles.ingredientsList}>
-                            <span className={styles.specificationValueFull}>
+                            <span className={styles.specificationValueFullIngredients}>
                                 {ingredients_list}  
                             </span>
                         </div>
