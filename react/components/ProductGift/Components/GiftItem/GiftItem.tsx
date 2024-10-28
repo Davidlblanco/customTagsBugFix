5526390dbc6f6ab3734219e567a99618c9786b5d
Walter Castillo - Senior WebDev <walter_castillo@siman.com>
2024-10-28 09:42:07 -0600

    feat(PDP): estilos para tag de regalías PDP

--- react/components/ProductGift/Components/GiftItem/GiftItem.tsx
+++ react/components/ProductGift/Components/GiftItem/GiftItem.tsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Gift } from "../../Types/productGiftTypes";
-import { RegaliaIcon } from "../../Assets/ant-design_gift-outlined";
+import { RegaliaIcon } from "../../Assets/ant-design_gift-white";
 import styles from "./styles.css";
 
-const GiftItem = ({ gift }: { gift: Gift }) => {
-   const filteredName = gift.skuName.replace(/\/\/.*/, "").trim();
+const GiftItem = ({ gifts }: { gifts: Gift[] }) => {
+
+   const [currentIndex, setCurrentIndex] = useState(0);
+
+   useEffect(() => {
+      const interval = setInterval(() => {
+         setCurrentIndex((prevIndex) =>
+            prevIndex === gifts.length - 1 ? 0 : prevIndex + 1
+         );
+      }, 3000);
+
+      return () => clearInterval(interval);
+   }, [gifts.length]);
 
    return (
       <div className={styles.containerGiftItem}>
          <div className={styles.leftGift}>
-            <div className={styles.leftTopGift}>
-               <RegaliaIcon />
-               <span className={styles.regaliaText}>Regalia</span>
-            </div>
-            <div className={styles.leftBottomGift}>
-               <span className={styles.giftName}>{filteredName}</span>
-            </div>
+            <RegaliaIcon className={styles.regaliaIcon} />
+         </div>
+         <div className={styles.centerGift}>
+            <span className={styles.regaliaText}>{gifts.length > 1 ? gifts.length + " regalías disponibles" : "Regalía"}</span>
+            {gifts.length > 1 &&gifts.map((gift, index) => (
+               <span className={`${styles.giftNameList} ${index === currentIndex ? styles.active : ''}`} key={index}
+               > ● {gift.productName.replace(/\/\/.*/, "").trim()}</span>
+            ))}
+            {gifts.length === 1 && (
+               <span className={styles.giftName}>{gifts[0].productName.replace(/\/\/.*/, "").trim()}</span>
+            )}
          </div>
          <div className={styles.rightGift}>
-            <img width={80} height={80} src={gift.images[0].imageUrl} alt={gift.images[0].imageText} />
+            {gifts.length > 1 && gifts.map((gift, index) => (
+               <div
+                  key={index}
+                  className={`${styles.carouselItem} ${index === currentIndex ? styles.active : ''
+                     }`}
+                  style={{ display: index === currentIndex ? 'block' : 'none' }}
+               >
+                  <img src={gift.images[0].imageUrl} alt={gift.images[0].imageText} className={styles.imgGift} />
+               </div>
+            ))}
+            {gifts.length === 1 && (
+               <img src={gifts[0].images[0].imageUrl} alt={gifts[0].images[0].imageText} className={styles.imgGift} />
+            )}
          </div>
       </div>
    );
