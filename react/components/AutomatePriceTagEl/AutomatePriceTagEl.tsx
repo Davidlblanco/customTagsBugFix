8d66322dab4a7dfcd1d5c4af79455a6e639e5a2a
Erycson José <erycson.silva@cadastra.com>
2025-02-25 10:00:46 -0300

    fix: fix duplicate tag not rendered

--- react/components/AutomatePriceTagEl/AutomatePriceTagEl.tsx
+++ react/components/AutomatePriceTagEl/AutomatePriceTagEl.tsx
@@ -8,140 +8,168 @@ import sleep from "../../helpers/sleep";
 import useProductPromotionsTags from "../../hooks/useProductPromotionsTags";
 
 import { useRenderSession } from "vtex.session-client";
-import { useRuntime } from 'vtex.render-runtime';
+import { useRuntime } from "vtex.render-runtime";
 
 import { useIntl } from "react-intl";
 import { formatCurrency } from "vtex.format-currency";
 
 interface AutomatePriceTagElProps {
-  visibility: "pdp" | "productSummary";
-  container: string;
-  positionTop: PositionProps;
-  positionCenter: PositionProps;
-  positionBottom: PositionProps;
-  algoliaProductContext?: AlgoliaProductContext;
+    visibility: "pdp" | "productSummary";
+    container: string;
+    positionTop: PositionProps;
+    positionCenter: PositionProps;
+    positionBottom: PositionProps;
+    algoliaProductContext?: AlgoliaProductContext;
 }
 
 type PositionProps = {
-  insert: "before" | "after";
-  class: string;
+    insert: "before" | "after";
+    class: string;
 };
 
 const AutomatePriceTagEl = ({
-  visibility,
-  container,
-  positionTop,
-  positionCenter,
-  positionBottom,
-  algoliaProductContext
+    visibility,
+    container,
+    positionTop,
+    positionCenter,
+    positionBottom,
+    algoliaProductContext,
 }: AutomatePriceTagElProps) => {
+    const { session } = useRenderSession();
+    const { account, workspace, culture } = useRuntime();
+    const intl = useIntl();
 
-  const { session } = useRenderSession();
-  const { account, workspace, culture } = useRuntime();
-  const intl = useIntl();
+    const { filteredTags, hrefProduct, skuId } = useProductPromotionsTags(
+        session,
+        account,
+        workspace,
+        algoliaProductContext
+    );
 
-  const { filteredTags, hrefProduct, skuId } = useProductPromotionsTags(session, account, workspace, algoliaProductContext);
+    const originalContainer = useRef<HTMLDivElement>(null);
 
-  const originalContainer = useRef<HTMLDivElement>(null);
+    const formatTotalCalculation = (configGroupPromotions: ConfigGroupPromotions[]) => {
+        const item = configGroupPromotions?.map((promotion) => {
+            const totalCalculationValue = promotion?.totalCalculation / 100 || 0;
+            const formattedTotalCalculation = formatCurrency({ intl, culture, value: totalCalculationValue });
 
-  const formatTotalCalculation = (configGroupPromotions: ConfigGroupPromotions[]) => {
-    const item = configGroupPromotions?.map((promotion) => {
-      const totalCalculationValue = (promotion?.totalCalculation / 100) || 0;
-      const formattedTotalCalculation = formatCurrency({ intl, culture, value: totalCalculationValue });
+            return {
+                ...promotion,
+                totalCalculation: formattedTotalCalculation,
+            };
+        });
+        return item;
+    };
 
-      return {
-        ...promotion,
-        totalCalculation: formattedTotalCalculation
-      };
-    });
-    return item;
-  }
+    const insertTags = async (
+        tagArray: ConfigGroupPromotions[],
+        containerSelector: string,
+        positionClass: PositionProps,
+        renderedTags: string,
+        tagsNotRendered: string
+    ) => {
+        const containers = document.querySelectorAll(containerSelector);
 
-  const insertTags = async (
-    tagArray: ConfigGroupPromotions[],
-    containerSelector: string,
-    positionClass: PositionProps,
-    renderedTags: string,
-    tagsNotRendered: string
-  ) => {
-    const containers = document.querySelectorAll(containerSelector);
-
-    if (!containers) {
-      return;
-    }
-
-    containers.forEach((container: Element) => {
-      if (container instanceof HTMLAnchorElement && visibility === "productSummary") {
-        if (!compareProductIdsInURL(hrefProduct, container.href)) {
-          return;
-        }
-      }
-
-      if (container.querySelector(`.${renderedTags}`)) {
-        const removeNotTag = container.querySelector(`.${tagsNotRendered}`);
-
-        if (removeNotTag) {
-          removeNotTag.remove();
-        }
-
-        return;
-      }
-
-      const elementWithPositionClass = container.querySelector(positionClass.class);
-
-      if (elementWithPositionClass) {
-        if (tagArray.length === 0 && visibility === "productSummary") {
-          if (container.querySelector(`.${tagsNotRendered}`)) {
+        if (!containers) {
             return;
-          }
-          const renderContainer = document.createElement("div");
-          renderContainer.classList.add(tagsNotRendered);
-          if (positionClass.insert === "before") {
-            elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass);
-          } else {
-            elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass.nextSibling);
-          }
-          return;
         }
 
-        if (tagArray.length === 0) {
-          return;
+        containers.forEach((container: Element) => {
+            if (container instanceof HTMLAnchorElement && visibility === "productSummary") {
+                if (!compareProductIdsInURL(hrefProduct, container.href)) {
+                    return;
+                }
+            }
+
+            if (container.querySelector(`.${renderedTags}`)) {
+                const removeNotTag = container.querySelector(`.${tagsNotRendered}`);
+
+                if (removeNotTag) {
+                    removeNotTag.remove();
+                }
+
+                return;
+            }
+
+            const elementWithPositionClass = container.querySelector(positionClass.class);
+
+            if (elementWithPositionClass) {
+                if (tagArray.length === 0 && visibility === "productSummary") {
+                    if (container.querySelector(`.${tagsNotRendered}`)) {
+                        return;
+                    }
+                    const renderContainer = document.createElement("div");
+                    renderContainer.classList.add(tagsNotRendered);
+                    if (positionClass.insert === "before") {
+                        elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass);
+                    } else {
+                        elementWithPositionClass?.parentNode?.insertBefore(
+                            renderContainer,
+                            elementWithPositionClass.nextSibling
+                        );
+                    }
+                    return;
+                }
+
+                if (tagArray.length === 0) {
+                    return;
+                }
+
+                const renderContainer = document.createElement("div");
+                renderContainer.classList.add(renderedTags);
+
+                if (positionClass.insert === "before") {
+                    elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass);
+                } else {
+                    elementWithPositionClass?.parentNode?.insertBefore(
+                        renderContainer,
+                        elementWithPositionClass.nextSibling
+                    );
+                }
+
+                ReactDOM.render(<RenderCustomTags tagArray={tagArray} visibility={visibility} />, renderContainer);
+            }
+        });
+    };
+
+    const addTags = useCallback(async () => {
+        if (!filteredTags) return;
+
+        insertTags(
+            formatTotalCalculation(filteredTags?.top),
+            container,
+            positionTop,
+            "tag-automate-price-top",
+            "tag-top-not-rendered"
+        );
+        insertTags(
+            formatTotalCalculation(filteredTags?.center),
+            container,
+            positionCenter,
+            "tag-automate-price-center",
+            "tag-center-not-rendered"
+        );
+        insertTags(
+            formatTotalCalculation(filteredTags?.bottom),
+            container,
+            positionBottom,
+            "tag-automate-price-bottom",
+            "tag-bottom-rendered"
+        );
+    }, [filteredTags, container, positionTop, positionCenter, positionBottom]);
+
+    useEffect(() => {
+        addTags();
+        sleep(500).then(addTags);
+    }, [addTags, skuId, originalContainer.current]);
+
+    useEffect(() => {
+        if (originalContainer.current?.parentElement) {
+            originalContainer.current.parentElement.style.height = "";
         }
+    }, [originalContainer]);
 
-        const renderContainer = document.createElement("div");
-        renderContainer.classList.add(renderedTags);
-
-        if (positionClass.insert === "before") {
-          elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass);
-        } else {
-          elementWithPositionClass?.parentNode?.insertBefore(renderContainer, elementWithPositionClass.nextSibling);
-        }
-
-        ReactDOM.render(<RenderCustomTags tagArray={tagArray} visibility={visibility} />, renderContainer);
-      }
-    });
-  };
-
-  const addTags = useCallback(async () => {
-    if (!filteredTags) return;
-
-    insertTags(formatTotalCalculation(filteredTags?.top), container, positionTop, "tag-automate-price-top", "tag-automate-price-top-not-rendered");
-    insertTags(formatTotalCalculation(filteredTags?.center), container, positionCenter, "tag-automate-price-center", "tag-automate-price-center-not-rendered");
-    insertTags(formatTotalCalculation(filteredTags?.bottom), container, positionBottom, "tag-automate-price-bottom", "tag-automate-price-top-bottom-rendered");
-  }, [filteredTags, container, positionTop, positionCenter, positionBottom]);
-
-  useEffect(() => {
-    addTags();
-    sleep(500).then(addTags)
-  }, [addTags, skuId, originalContainer.current]);
-
-  useEffect(() => {
-    if (originalContainer.current?.parentElement) {
-      originalContainer.current.parentElement.style.height = "";
-    }
-  }, [originalContainer]);
-
-  return <div ref={originalContainer}></div>;
+    return <div ref={originalContainer}></div>;
 };
 
 export default AutomatePriceTagEl;
