a8b7d8b90a4ef3796099911ada1dd94a630c66d2
Kevin Garcia <kevinede40@gmail.com>
2025-04-29 15:16:14 -0600

    feat: estructura de componente y estilos

--- react/components/CookiesAgreement/CookiesAgreement.js
+++ react/components/CookiesAgreement/CookiesAgreement.js
@@ -1,33 +1,42 @@
-import React from "react";
-import "./Cookies.css";
+import React, { useEffect, useState } from "react";
+import styles from "./index.css";
 
 function CookiesAgreement() {
-    const myClass = "vtex-flex-layout-0-x-flexRow--cookies_modal";
+    const [showComponent, setShowComponent] = useState(false);
+
+    useEffect(() => {
+        const cookiesAccepted = localStorage.getItem("cookies") === "true";
+        setShowComponent(!cookiesAccepted);
+    }, []);
 
     function savePreference() {
         localStorage.setItem("cookies", "true");
-        document.getElementsByClassName(myClass)[0].style.display = "none";
+        setShowComponent(false);
     }
 
-    if (localStorage.getItem("cookies") === "true") {
+    if (!showComponent) {
         return null;
-    } else {
-        return (
-            <div className={myClass} id="cookiesAgreement">
-                <div>
-                    <h2>Este sitio web utiliza cookies</h2>
-                    <p>
-                        En siman.com utilizamos cookies y recopilamos ciertos datos personales para mejorar tu
-                        experiencia de navegación, personalizar contenidos y analizar el uso del sitio. Al continuar
-                        navegando, aceptas nuestra <a href="/privacidad">Política de Privacidad</a> y el uso de cookies.
-                    </p>
-                    <button className="vtex-flex-layout-0-x-flexRow--cookies_button" onClick={savePreference}>
-                        Aceptar
-                    </button>
-                </div>
-            </div>
-        );
     }
+
+    return (
+        <div className={styles.cookiesModal} id="cookiesAgreement">
+            <div>
+                <h2>Este sitio web utiliza cookies</h2>
+                <p>
+                    En siman.com utilizamos cookies y recopilamos ciertos datos personales para mejorar tu experiencia
+                    de navegación, personalizar contenidos y analizar el uso del sitio. Al continuar navegando, aceptas
+                    nuestra{" "}
+                    <a id={styles.cookiesAgreementLink} href="/privacidad">
+                        Política de Privacidad
+                    </a>{" "}
+                    y el uso de cookies.
+                </p>
+                <button className={styles.cookiesButton} onClick={savePreference}>
+                    Aceptar
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default CookiesAgreement;
